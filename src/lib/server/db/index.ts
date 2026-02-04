381e1bb3413bf6cd4be4d1cef00eec3aba0e1895
SuperLonci <55552282+SuperLonci@users.noreply.github.com>
2026-02-04 22:51:45 +0100

    Move drizzle dependency

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,6 +3,7 @@ import Database from 'better-sqlite3';
 import * as schema from './schema';
 import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
 if (!env.DATABASE_URL && !building) {
     throw new Error('DATABASE_URL is not set');
@@ -18,5 +19,14 @@ if (!building) {
     sqlite = new Database(':memory:'); 
 }
 
-
 export const db = drizzle(sqlite, { schema });
+
+if (!building) {
+    try {
+        console.log("⏳ Running migrations...");
+        migrate(db, { migrationsFolder: "drizzle" });
+        console.log("✅ Migrations complete.");
+    } catch (err) {
+        console.error("❌ Migration failed:", err);
+    }
+}
